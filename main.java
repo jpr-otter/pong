d37f693c57c31ad7b160efa7cf1d3ddb9f05d472
jpr-otter <96299330+jpr-otter@users.noreply.github.com>
2023-03-16 18:25:01 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+package mikami;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new Gui();
+        new Var();
+        new Movement();
+        new MovementOppo();
+        new BallMovement();
+        new BallCollision();
+
+    }
+}
